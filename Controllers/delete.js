29d8a751a407ed2caf87c0c4ebdf2139af29717d
Peerapat <85300665+peerapatsiriai@users.noreply.github.com>
2022-05-04 20:26:47 +0700

    .

--- Controllers/delete.js
+++ Controllers/delete.js
@@ -3,10 +3,14 @@ const ConnectDB = require('../conDB')
 
 module.exports.deleteUser =  async(req, res) => {
     const {id} = req.body
-    console.log(id);
     const DB = new Client(ConnectDB);
     await DB.connect();
-    await DB.query(`DELETE FROM product WHERE id = '${id}'`);
-    await DB.end();
-    res.status(200).json({message: 'Deleteed'});
+    const user = await DB.query(`SELECT * FROM product WHERE id = '${id}'`);
+    if(user.rows.length <= 0) {
+        return res.status(404).json({message: 'NotFoundUser'})
+    }else{
+        await DB.query(`DELETE FROM product WHERE id = '${id}'`);
+        await DB.end();
+        return res.status(200).json({message: 'Deleteed'});
+    }
 }
