9c7b6d11e9651cda86f17b952e3b1a1a7e8f7689
Peerapat <85300665+peerapatsiriai@users.noreply.github.com>
2022-11-18 13:28:02 +0700

    delete pass

--- conDB.js
+++ conDB.js
@@ -1,8 +1,8 @@
 module.exports = credentials = {
-    user: "kmuayaur",
-    host: "satao.db.elephantsql.com",
-    database: "kmuayaur",
-    password: "aduEt_Z8VWaTMYdVhNTXc4rZmA5ajs2R",
-    port: 5432,
+    user: "",
+    host: "",
+    database: "",
+    password: "",
+    port: ,
   };
   
